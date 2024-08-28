ca9ff5c407d801ddf3e10a41ed4ce91fb5c2c624
AnthonyFuFu <s9017688@yahoo.com.tw>
2024-08-28 12:25:07 +0800

    feat:QA3

--- src/main/java/com/yahooInterview/APIQA/QA3.java
+++ src/main/java/com/yahooInterview/APIQA/QA3.java
@@ -1,5 +1,12 @@
 package com.yahooInterview.APIQA;
 
 public class QA3 {
-
+	
+//	REST的核心原則
+//	資源
+//	無狀態性
+//	客戶端
+//	統一接口
+	
+	
 }
--- src/main/java/com/yahooInterview/javaQA/QA3.java
+++ src/main/java/com/yahooInterview/javaQA/QA3.java
@@ -2,8 +2,21 @@ package com.yahooInterview.javaQA;
 
 public class QA3 {
 	
-//	訪問類 public private protected default
+//	封裝類型修飾字public、protected、default、private
+//	public：任何地方都可以訪問到
+//	protected：同個套件下的class或是其子類別
+//	default：同個套件下的class都可以訪問到
+//	private：只能在同個class下能訪問到
 	
-//	非訪問類：static, final, abstract, synchronized
+//	Java關鍵字
+//	static：class在被呼叫時預讀取該方法或建構子
+//	final：一旦被賦值後就無法更改
+//	abstract：抽象概念物件無法被實體化，必須以子類別做實作去實現方法
+//	synchronized：同一時間只有一線程能夠執行，表示同步程式碼做同步控制
+//	interface：
+//	volatile：
+//	transient：
+//	native：
+//	strictfp：
 	
 }
