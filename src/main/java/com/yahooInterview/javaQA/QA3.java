96e88bfa86c4b7f44e8f8f19accf82d6ba8ded14
AnthonyFuFu <s9017688@yahoo.com.tw>
2024-08-29 17:29:46 +0800

    feat:QA3 update

--- src/main/java/com/yahooInterview/javaQA/QA3.java
+++ src/main/java/com/yahooInterview/javaQA/QA3.java
@@ -13,10 +13,11 @@ public class QA3 {
 //	final：一旦被賦值後就無法更改
 //	abstract：抽象概念物件無法被實體化，必須以子類別做實作去實現方法
 //	synchronized：同一時間只有一線程能夠執行，表示同步程式碼做同步控制
-//	interface：
-//	volatile：
-//	transient：
-//	native：
-//	strictfp：
+//	interface：這是一個抽象類型，目的是統一接口，以實做此接口的class去實作方法
+//	implements：這是一個實做類型，實做interface用
+//	volatile：可確保資料可見性，一個thread更新變數，其他thread也會重新更新內部變數。不保證atomic，不可用於計數操作。通常被稱為輕量級的synchronized，但只用在變數不用在class或方法
+//	transient：當串列化某個物件時，如果該物件的某個變數是transient，那麼這個變數就不會被串列化進去。儲存到磁碟上時，實際上transient變數的值是不會儲存的。
+//	native：以java中调用本地的方法（通常是用 C 或 C++ 協作時所用）
+//	strictfp：即strict float point (精確浮點)方法中所有的float和double表達式都嚴格遵守FP-strict的限制,符合IEEE-754規範。
 	
 }
